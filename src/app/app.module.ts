e5927471f25828516f26f9b39edcb08ec8259a1d
hparra07 <howard.parra95@gmail.com>
2022-06-22 00:32:04 -0300

    agregando animaciones y servicios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule} from '@angular/common/http'
+//  Rutas
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { PortafolioComponent } from './pages/portafolio/portafolio.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ItemComponent } from './pages/item/item.component';
-import { AppRoutingModule } from './app-routing.module';
 import { ContactComponent } from './pages/contact/contact.component';
 
 @NgModule({
@@ -22,7 +24,8 @@ import { ContactComponent } from './pages/contact/contact.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/interfaces/info-pagina.interface.ts
@@ -0,0 +1,13 @@
+
+
+export interface InfoPagina{
+  titulo?: string;
+  email?: string;
+  nombre_corto?: string;
+  pagina_autor?: string;
+  instagram?: string;
+  twitter?: string;
+  linkedin?: string;
+  github?: string;
+  equipo_trabajo?: any[];
+}
